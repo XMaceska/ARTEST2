ccda082e1eeea44bd00da0d9be939576b560334d
XMaceska <dave.maceska@seznam.cz>
2020-11-24 23:41:38 +0100

    pridan church a domy

--- script.js
+++ script.js
@@ -22,14 +22,14 @@ var models = [
     {
         url: './assets/magnemite/scene.gltf',
         scale: '0.5 0.5 0.5',
-        info: 'Magnemite, Lv. 5, HP 10/10',
+        info: 'Kostel Sv. Mourenec interier',
         rotation: '0 180 0',
     },
     {
         url: './assets/articuno/scene.gltf',
         scale: '0.2 0.2 0.2',
         rotation: '0 180 0',
-        info: 'Articuno, Lv. 80, HP 100/100',
+        info: 'Již neexistující domu v okolí',
     },
     {
         url: './assets/dragonite/scene.gltf',
